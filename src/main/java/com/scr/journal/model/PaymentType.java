dbc24df6389fb526f7af85917e794f92bfcea79f
Scipi <rolandk@fokus.hu>
2018-03-04 21:24:15 +0100

    Added display value for PaymentType enums

--- src/main/java/com/scr/journal/model/PaymentType.java
+++ src/main/java/com/scr/journal/model/PaymentType.java
@@ -1,6 +1,30 @@
 package com.scr.journal.model;
 
 public enum PaymentType {
-    CASH,
-    BANK_TRANSFER
+    CASH("cash"),
+    BANK_TRANSFER("bank transfer");
+
+    private final String associatedValue;
+
+    PaymentType(String associatedValue) {
+        this.associatedValue = associatedValue;
+    }
+
+    public String getAssociatedValue() {
+        return associatedValue;
+    }
+
+    @Override
+    public String toString() {
+        return getAssociatedValue();
+    }
+
+    public static PaymentDirection tryParse(String value) {
+        for (PaymentDirection pd : PaymentDirection.values()) {
+            if (pd.getAssociatedValue().equalsIgnoreCase(value)) {
+                return pd;
+            }
+        }
+        return null;
+    }
 }
